861a8a2be463b2af4c64d351b33c48d9b379e71b
Gowa2017 <33367355+Gowa2017@users.noreply.github.com>
2021-10-18 19:49:18 +0800

    typofix

--- core/RoomManager.lua
+++ core/RoomManager.lua
@@ -6,8 +6,8 @@ function M:_init() self.rooms = {} end
 
 function M:getRoom(entityRef) return self.rooms[entityRef] end
 
-function M:addRoom(room) self.rooms[room.entityRef] = room end
+function M:addRoom(room) self.rooms[room.entityReference] = room end
 
-function M:removeRoom(room) self.rooms[room.entityRef] = nil end
+function M:removeRoom(room) self.rooms[room.entityReference] = nil end
 
 return M
