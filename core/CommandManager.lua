7801a0885ace8914b6cca9090e276dd6337e36c6
Gowa2017 <33367355+Gowa2017@users.noreply.github.com>
2021-10-19 21:23:44 +0800

    modify command manager

--- core/CommandManager.lua
+++ core/CommandManager.lua
@@ -1,8 +1,8 @@
 local class  = require("pl.class")
+local tablex = require("pl.tablex")
 ---@class CommandManager : Class
 local M      = class()
 
-local tablex = require "pl.tablex"
 function M:_init() self.commands = {} end
 
 function M:get(command) return self.commands[command] end
@@ -10,17 +10,17 @@ function M:get(command) return self.commands[command] end
 function M:add(command)
   self.commands[command.name] = command
   if command.aliases then
-    tablex.foreach(command.aliases,
-                   function(alias, i) self.commands[alias] = command end)
+    tablex.foreachi(command.aliases,
+                    function(alias, _) self.commands[alias] = command end)
   end
 end
-
 function M:remove(command) self.commands[command.name] = nil end
 
-function M:find(search)
-  for name, command in pairs(self.commands) do
-    local s, _ = name:find(search)
-    if s == 1 then return command end
+function M:find(search, returnAlias)
+  for name, command in ipairs(self.commands) do
+    if name:find(search) == 1 then
+      return returnAlias and { command = command, alias   = name } or command
+    end
   end
 end
 
